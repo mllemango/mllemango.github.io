504a306183e3d333ada1ed4684fcbb0a98f24082
Melanie Wang <melanie.wang@shopify.com>
2024-05-05 22:39:47 -0400

    Use SPA defaults to test deployment

---
+++ app/routes/_index.tsx
@@ -0,0 +1,22 @@
+import type { MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix SPA" },
+    { name: "description", content: "Welcome to Remix (SPA Mode)!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+      <h1>Welcome to Remix (SPA Mode)</h1>
+      <ul>
+        <li>
+          <Link to="form-example">Form Example</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
